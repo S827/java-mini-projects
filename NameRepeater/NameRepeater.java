c179d83894859ec4dbf9813028657b8b85258732
s827 <dskumar827@gmail.com>
2025-12-02 00:41:19 +0530

    feat: add NameRepeater project

---
+++ NameRepeater/NameRepeater.java
@@ -0,0 +1,14 @@
+package NameRepeater;
+import java.util.Scanner;
+public class NameRepeater {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.println(name);
+        }
+    }
+}
